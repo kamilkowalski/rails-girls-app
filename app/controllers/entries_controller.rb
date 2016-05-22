4da00d94415f9c263ae84e001c12ef2baf3e9022
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-05-22 13:22:21 +0000

    Implement editing and destroying, add Bootstrap

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,35 @@ class EntriesController < ApplicationController
     def show
         @entry = Entry.find(params[:id])
     end
+    
+    def new
+    end
+    
+    
+    def create
+      entry = Entry.create(entry_params)
+      redirect_to(entry_path(entry))
+    end
+    
+    def edit
+        @entry = Entry.find(params[:id])
+    end
+    
+    def update
+      entry = Entry.find(params["id"])
+      entry.update(entry_params)
+      redirect_to(entry_path(entry))
+    end
+    
+    def destroy
+       entry = Entry.find(params[:id])
+       entry.destroy
+       redirect_to entries_path
+    end
+    
+    private
+
+    def entry_params
+      params.require(:entry).permit(:title, :contents)
+    end
 end
